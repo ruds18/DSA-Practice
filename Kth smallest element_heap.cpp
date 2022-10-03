232c7694e76fa4667accb665750ad9454775e653
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-10-03 22:16:40 +0530

    Kth smallest element_heap

---
+++ Kth smallest element_heap.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+      //Using concept of Max heap
+      priority_queue<int>pq;
+      
+      for(int i=0;i<=r;i++){
+          pq.push(arr[i]);
+          if(pq.size()>k){
+              pq.pop();
+          }
+      }
+        return pq.top();
+    }
+};
