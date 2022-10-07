597499a28c2a3d9d06c65972f7df41a15b235f15
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-10-07 13:34:42 +0530

    Time: 3 ms (82.44%), Space: 11.7 MB (60.57%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
-    int fillCups(vector<int>& arr) {
-        int sec=0;
+    int fillCups(vector<int>& a) {
+       return max({a[0], a[1], a[2] , (a[0] + a[1] + a[2] + 1) / 2});
+    }
+};
+
+
+/* 
+ int sec=0;
         sort(arr.begin(), arr.end());
         while(arr[2] > 0){
             arr[2]--;
@@ -9,6 +15,4 @@ public:
             if(arr[1] > 0) arr[1] --;
               sort(arr.begin(), arr.end());
         }
-        return sec;
-    }
-};
+        return sec;*/
