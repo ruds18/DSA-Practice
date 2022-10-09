fc6d6b04ed5a801f615632a12402fd873809d6b3
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-10-09 20:28:28 +0530

    Time: 127 ms (25.00%), Space: 38.8 MB (25.00%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -3,7 +3,7 @@ public:
     int hardestWorker(int n, vector<vector<int>>& logs) {
         int end_work = 0, max_d =0, id=0;
         
-            for(auto &l : logs){
+            for(auto l : logs){
                 if(max_d <= l[1] - end_work){
                     id = max_d < l[1]-end_work ? l[0]  : min(id,l[0]);
                     max_d= l[1]-end_work;
