9e9e5125a5a592f57732dc65f17f308a30f4360f
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-10-21 14:05:03 +0530

    Time: 425 ms (31.28%), Space: 77.2 MB (60.05%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,12 +4,11 @@ public:
         unordered_map<int, int>mp;
         
         for(int i=0 ;i<nums.size();i++){
-            // not present in container
-            if(mp.count(nums[i]) == 0)
-                mp[nums[i]] = i;
-            else if(i - mp[nums[i]] <= k)
-                return true;
-            else
+            //  present in container
+            if(mp.count(nums[i]) == 1){
+                int diff = abs(i - mp[nums[i]]);
+                if(diff <=k ) return true;
+            }
                 mp[nums[i]] = i;
         }
         return false;
