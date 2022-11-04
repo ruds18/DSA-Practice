35849281cade4555ecc8d02221cc07e4fa782392
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-11-04 18:38:57 +0530

    Time: 23 ms (64.29%), Space: 22 MB (7.36%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,41 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// left right val
 class Solution {
 public:
-    int height(TreeNode* &root){
-        //base case
-        if(root == NULL) return 0;
-        
-        int lh = height(root->left);
-        int rh = height(root->right);
-        
-        return max(lh, rh) + 1;
-    }
-    bool isBalanced(TreeNode* root) {
-        // lh + rh <=1 
+    pair<bool , int> isBalancedFast(TreeNode* root){
         //edge case
-      if(root == NULL) return true;
-      
-      bool left = isBalanced(root->left);
-      bool right = isBalanced(root->right);
-      bool diff = abs (height(root->left) - height(root->right)) <= 1 ;
-      
-     
-       if(left && right && diff){
-           return true ;
-       }else{
-           return false;
-       }
-      
+        if(root == NULL) {
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
         
+        pair<bool,int> left = isBalancedFast(root->left);
+        pair<bool,int> right = isBalancedFast(root->right);
+        
+        bool ls = left.first;
+        bool rs = right.first;
+        bool diff = abs(left.second - right.second)  <=1 ; 
+        
+        pair<bool ,int> ans;
+        ans.second = max(left.second , right.second) +1;
+        if(ls && rs && diff){
+            ans.first = true;
+        }else{
+            ans.first = false;
+        }
+        return ans;
+    };
+    bool isBalanced(TreeNode* root) {
+      return isBalancedFast(root).first;
     }
 };
