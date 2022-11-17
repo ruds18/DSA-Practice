3e196c7223d42c529638341757b9f81a3d90b9fb
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-11-17 15:56:23 +0530

    Time: 2 ms (44.08%), Space: 6.1 MB (61.23%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double>ans;
+        
+        ans.push_back(celsius+273.15);
+        ans.push_back(celsius*1.80+32.00);
+        
+        return ans;
+        
+    }
+};
