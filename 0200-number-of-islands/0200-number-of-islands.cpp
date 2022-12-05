70bea3cddf0ce08fb2f42f69d1eba27cbf33a910
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-12-05 23:46:44 +0530

    Time: 112 ms (17.93%), Space: 19.3 MB (13.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,6 +1,6 @@
 class Solution {
     private:
-    void bfs(int nodeRow, int nodeCol,vector<vector<int>> &visited,vector<vector<char>>& grid , int delRow[], int delCol[]){
+   void bfs(int nodeRow, int nodeCol,vector<vector<int>> &visited,vector<vector<char>>& grid , int delRow[], int delCol[]){
         visited[nodeRow][nodeCol] = 1;
         queue<pair<int,int>>q;
         q.push({nodeRow,nodeCol});
@@ -24,21 +24,22 @@ class Solution {
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n= grid.size();
+        int n = grid.size();
         int m = grid[0].size();
-        int delRow[] = {-1, 0 , +1, 0};
-        int delCol[] = {0 , +1 , 0 , -1};
+        int deltaRow[] = {-1, 0, +1, 0};
+        int deltaCol[] = {0, +1, 0, -1};
+        vector<vector<int>>vis(n , vector<int>(m, 0));
+        int ans =0;
         
-        vector<vector<int>>visited(n, vector<int>(m,0));
-        int cnt =0;
-        for(int row =0 ;row<n;row++){
-            for(int col = 0; col<m ;col++){
-                if(visited[row][col] != 1 && grid[row][col] == '1'){
-                    cnt++;
-                    bfs(row, col, visited, grid, delRow, delCol);
+        // bfs
+        for(int row = 0 ; row< n; row++){
+            for(int col =0; col<m ; col++){
+                if(!vis[row][col] && grid[row][col] ==  '1'){
+                    ans++;
+                    bfs(row, col, vis, grid, deltaRow, deltaCol);
                 }
             }
         }
-        return cnt;
+        return ans;
     }
 };
