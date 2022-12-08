614db5d4e6d39b5e153b8d3946150c4914675d91
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-12-08 18:32:58 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -31,8 +31,10 @@ class Solution {
     int numberOfEnclaves(vector<vector<int>> &grid) {
         int n = grid.size();
         int m = grid[0].size();
+        int deltaRow[] = {-1, 0, +1, 0};
+        int deltaCol[] = {0, +1, 0, -1};
         int cnt =0;
-        
+        queue<pair<int, int>>q;
         vector<vector<bool>>vis(n , vector<bool>(m, false));
         
         // Boundary Traversal
@@ -40,24 +42,46 @@ class Solution {
         for(int col =0 ;col<m; col++){
             // first row
             if(!vis[0][col] && grid[0][col] == 1){
-                dfs(0, col, vis, grid);
+                vis[0][col] = true;
+                q.push({0, col});
             }
             //last row
             if(!vis[n-1][col] && grid[n-1][col] == 1){
-                dfs(n-1, col, vis, grid);
+                vis[n-1][col] = true;
+                q.push({n-1, col});
             }
         }
         
           for(int row =0 ;row<n; row++){
             // first row
             if(!vis[row][0] && grid[row][0] == 1){
-                dfs(row, 0, vis, grid);
+                vis[row][0] = true;
+                q.push({row, 0});
             }
             //last row
             if(!vis[row][m-1] && grid[row][m-1] == 1){
-                dfs(row, m-1, vis, grid);
+                vis[row][m-1] = true;
+                q.push({row, m-1});
             }
         }
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int k=0; k<4; k++){
+            int nrow = row + deltaRow[k];
+            int ncol = col + deltaCol[k];
+            
+             if(nrow >= 0 && nrow <n && ncol >=0 && ncol <m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                q.push({nrow, ncol});
+                vis[nrow][ncol] = true;
+            }
+            }
+        }
+        
+    
         for(int i=0; i<n;i++){
             for(int j=0;j<m;j++){
                 if(!vis[i][j] && grid[i][j] == 1) cnt++;
