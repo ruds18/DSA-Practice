c20aae05987202433f9dacd6c65217fe25a6e320
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-12-14 00:46:20 +0530

    Time: 623 ms (16.87%), Space: 118.1 MB (56.05%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,53 @@
+
+class Solution {
+    private:
+    ListNode* getMid(ListNode* &head){
+        //Hare tortoise approach
+        ListNode* fast = head->next; // 2 Steps
+        ListNode* slow = head; // 1 Step
+        
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    
+    ListNode* reverse(ListNode* head){
+        ListNode* cur = head;
+        ListNode* prev = NULL;
+        ListNode* nxt = NULL;
+        
+        while(cur){
+            nxt = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+        return prev;
+    }
+public:
+    bool isPalindrome(ListNode* head) {
+        //edge case
+        if(head->next == NULL && head) return true;
+        
+        //Step 1 - Find the middle element
+        ListNode* middle = getMid(head);
+        
+        //Step 2  - Reverse the other half of the array
+        ListNode* temp = middle->next;
+        middle->next = reverse(temp);
+        
+        //Step 3 - Compare both the halves
+        ListNode* index1 = head;
+        ListNode* index2 = middle->next;
+        
+        while(index2){
+            if(index1->val != index2->val) return false;
+            index1 = index1->next;
+            index2 = index2->next;
+            
+        }
+        return true;
+    }
+};
