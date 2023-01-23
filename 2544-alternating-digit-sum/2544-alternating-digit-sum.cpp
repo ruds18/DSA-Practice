e7d303c92bfbef2dc160e7540bf947e2dfc1bbd5
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-01-24 00:01:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     
-    void getSum(int n , int &sum , bool&flag){
+    void getSum(int n , int &sum , bool &sign){
         //base case
         if(!n) return;
+        
         int digit = n%10;
-        n= n/10;
-        //rr
-        getSum(n , sum,flag);
-        //vapas aate hue
-        //backtrack
-        if(flag) sum +=digit ; else sum -=digit;
-        flag = !flag;
+        n = n/10;
+        
+        // rr
+        getSum(n , sum, sign);
+        // vapas aate hue
+        if(sign) sum+= digit; else sum -= digit;
+        sign =!sign;
+        
     }
-    
     int alternateDigitSum(int n) {
+        bool sign = true;
         int sum =0;
-        bool flag = true;
-        getSum(n , sum,flag);
+        getSum(n , sum , sign);
         return sum;
     }
 };
