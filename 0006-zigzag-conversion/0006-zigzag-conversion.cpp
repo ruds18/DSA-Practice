54936e409e64c601169319b86d8ae8376860bdec
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-02-03 22:24:49 +0530

    Time: 7 ms (91.77%), Space: 8.3 MB (82.81%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
+        //corner case
         if(numRows == 1) return s;
-        
         int cycle = 2*numRows -2;
-        string ans="";
-        for(int i=0; i<numRows;i++){
-            for(int j=i; j<s.length(); j+=cycle){
+        string ans ="";
+        // rows
+        for(int i=0; i<numRows ;i++){
+            // accessing all the elements according to the row
+            for(int j=i; j<s.length(); j += cycle)
+            {
                 ans += s[j];
                 
-                int k= j+cycle-2*i;
-                if(i != 0 && i!= numRows-1 && k<s.length()){
+                int k = j+cycle-2*i;
+                if(i != 0 &&  i != numRows-1 && k < s.length()){
                     ans += s[k];
                 }
             }
