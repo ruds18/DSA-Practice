57c67fee97b03667e8f2f5ae4c20c67c1fec4592
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-02-18 16:40:57 +0530

    Time: 28 ms (34.99%), Space: 18 MB (70.22%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,11 +3,14 @@ public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin() , nums.end());
         //corner case
-    if(nums[nums.size()-1] != nums.size()) return nums.size();
+  if(nums[nums.size()-1] != nums.size()) return nums.size();
 
         int ans =-1;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] != i){ ans = i ; break;} 
+            if(nums[i] != i){ 
+                ans = i;       
+                break;
+           } 
         }
         return ans;
     }
