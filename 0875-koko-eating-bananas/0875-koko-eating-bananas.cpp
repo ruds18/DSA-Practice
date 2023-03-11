501eaa93d109fae64995548e84734b11bebe680a
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-03-11 16:19:44 +0530

    Time: 54 ms (39.24%), Space: 35.2 MB (5.00%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+    bool isPossible(vector<int>piles , int mid  , int H){
+        int total = 0;
+        for (int p : piles) 
+                total += (p + mid - 1) / mid;
+            if (total > H)
+                return false;
+            else
+                return true;
+    }
+    
+    
    int minEatingSpeed(vector<int>& piles, int H) {
         int l = 1, r = 1000000000;
+        int ans =0;
         while (l < r) {
             int m = (l + r) / 2, total = 0;
-            for (int p : piles)
-                total += (p + m - 1) / m;
-            if (total > H)
-                l = m + 1;
-            else
+            if(isPossible(piles , m, H)){
+                ans = m;
                 r = m;
+            }else{
+                l = m + 1;
+            }
+            
         }
         return l;
     }
