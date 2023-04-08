e90675a4d6b615bee57d946c0387166e2c83c6c4
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-04-08 19:12:54 +0530

    Time: 7 ms (61.34%), Space: 8.5 MB (87.83%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,37 +1,37 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
 public:
-    unordered_map<Node*, Node*>mp;
+    unordered_map<Node* , Node*> mp; // declaring map, to check whwther we have a copy of node or not and also to store copy
+    
     Node* cloneGraph(Node* node) {
-      if(node == NULL) return NULL;
-                
-        if(mp.find(node)==mp.end()){
-            mp[node]  = new Node(node->val , {});
+        if(node == NULL) // if node is null, then simply return null
+        {
+            return NULL;
+        }
+        
+        Node* first = new Node(node -> val, {}); // make a copy of first node
+        mp[node] = first;
+        
+        queue<Node*> q; //For bfs, we create queue
+        q.push(node); // push into queue
+        
+        while(q.empty() == false) // until q. empty == false
+        {
+            Node* curr = q.front(); // extract front node
+            q.pop(); // pop that from queue
             
-            for(auto adj : node->neighbors){
-                mp[node]->neighbors.push_back(cloneGraph(adj));
+            for(auto adj: curr -> neighbors) // now travel in adjcant
+            {
+                if(mp.find(adj) == mp.end()) // if not present in map
+                {
+                    mp[adj] = new Node(adj -> val, {}); // then create copy
+                    q.push(adj); // push nto the queue
+                    
+                }
+                
+                mp[curr] -> neighbors.push_back(mp[adj]); // in current node push adjcant node
             }
         }
+        
         return mp[node];
     }
 };
