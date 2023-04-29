4a894a3064937f14720856b1ed6b3de88825dd3c
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-04-30 02:40:03 +0530

    Time: 804 ms (5.10%), Space: 390.7 MB (5.19%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>&dp,vector<vector<int>>&p,int k , int i){
+        //base case
+        if(i>=p.size()) return 0;
+        if(dp[i][k] != -1) return dp[i][k];
+        
+        int maxi=0;
+        maxi = max(maxi,solve(dp ,p,k,i+1));
+        
+        for(int j=0;j<p[i].size();j++){
+            if(j+1<=k){
+                maxi = max(maxi,p[i][j] + solve(dp ,p,k-j-1 , i+1));
+            }else{
+                break;
+            }
+        }
+        return dp[i][k] = maxi;
+    }
+    
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        //prefix sum
+        for(int i = 0;i<piles.size();i++){
+            for(int j = 1; j <piles[i].size();j++){
+                piles[i][j] = piles[i][j] + piles[i][j-1];
+            }
+        }
         int n = piles.size();
-        vector dp(n + 1, vector<int>(k + 1, -1));
-        
-        function<int(int, int)> f = [&](int i, int coins) {
-            if (i == 0) {
-                return 0;
-            }
-            if (dp[i][coins] != -1) {
-                return dp[i][coins];
-            }
-            int currentSum = 0;
-            for (int currentCoins = 0; currentCoins <= min((int)piles[i - 1].size(), coins); currentCoins++) {
-                if (currentCoins > 0) {
-                    currentSum += piles[i - 1][currentCoins - 1];
-                }
-                dp[i][coins] = max(dp[i][coins], f(i - 1, coins - currentCoins) + currentSum);
-            }
-            return dp[i][coins];
-        };
-        
-        return f(n, k);
+            vector<vector<int>>dp(1001 , vector<int>(2001 , -1));
+
+
+        return solve(dp,piles,k,0);
     }
 };
