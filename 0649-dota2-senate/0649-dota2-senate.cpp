22428f7558b50a5d959a0f28727d62b2e32c591c
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-05-04 22:15:27 +0530

    Time: 6 ms (83.21%), Space: 7.3 MB (71.76%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int count =0, len=0;
+        
+        while(len != senate.size()){
+            string s;
+            len = senate.size();
+            for(int i=0;i<len;i++){
+                if(senate[i] == 'R'){
+                    if(count++  >=0){
+                         s+= 'R';
+                    };
+                }
+                if(senate[i] == 'D'){
+                    if(count-- <=0) {
+                        s+= 'D';
+                    }
+                }
+            }
+            swap(s,senate);
+        }
+        if(senate[0] == 'R') return "Radiant"; else return "Dire";
+    }
+};
