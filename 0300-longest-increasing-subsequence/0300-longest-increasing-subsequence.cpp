d74548c888f736a63ab816c41f800a9f8b489dd6
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-05-07 13:22:50 +0530

    Time: 11 ms (83.08%), Space: 10.3 MB (92.11%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int  n = nums.size();
-        // tabulation
-        vector<vector<int>>dp(n+1 , vector<int>(n+1,0));
-        
-        for(int index = n-1 ; index >= 0 ; index --){
-            for(int prev_index = index-1 ; prev_index >= -1 ; prev_index--){
-                int not_take = 0 + dp[index+1][prev_index+1];
-                int take  = INT_MIN;
-                if(prev_index == -1 || nums[index] > nums[prev_index]){
-                    take =  1 + dp[index+1][index+1];
-                }
-                dp[index][prev_index+1] = max(take,not_take);
+        //binary search
+        int n = nums.size();
+        vector<int>subSequence;
+        subSequence.push_back(nums[0]);
+        for(int i=1; i<n;i++){
+            if(nums[i] > subSequence.back())subSequence.push_back(nums[i]);
+            else{
+                int index = lower_bound(subSequence.begin(),subSequence.end() , nums[i] ) - subSequence.begin();
+                subSequence[index] = nums[i];
             }
         }
-        return dp[0][-1+1];
+        return subSequence.size();
     }
 };
