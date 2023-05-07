bfa9d9fbcfda5c6c360cf5ece74659d69ce815a9
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-05-07 15:08:38 +0530

    Time: 371 ms (27.78%), Space: 117.8 MB (69.44%) - LeetHub

--- 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -4,16 +4,18 @@ public:
         int n = obstacles.size();
         
         // lis[i] records the lowest increasing sequence of length i + 1.
-        vector<int> answer(n, 1), lis;
+        vector<int> answer(n), lis;
+        lis.push_back(obstacles[0]);
+        answer[0] = 1;
         
-        for (int i = 0; i < n; ++i) {
+        for (int i = 1; i < n; ++i) {
             // Find the rightmost insertion position idx.
             int idx = upper_bound(lis.begin(), lis.end(), obstacles[i]) - lis.begin();
-            if (idx == lis.size())
+            if (obstacles[i] >= lis.back())
                 lis.push_back(obstacles[i]);
             else
                 lis[idx] = obstacles[i];
-            answer[i] = idx + 1;
+            answer[i] = idx+1;
         }
         return answer;
     }
