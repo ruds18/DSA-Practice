f25cf575f21bb3770b2c638804fe9b6b9599bf48
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-05-09 01:40:37 +0530

    Added solution - LeetHub

--- Help Classmates - GFG/help-classmates.cpp
+++ Help Classmates - GFG/help-classmates.cpp
@@ -13,18 +13,19 @@ class Solution{
     public:
     vector<int> help_classmate(vector<int> arr, int n) 
     { 
-        vector<int>ans;
-        for(int i=0; i<n;i++){
-            int position =-1;
-            for(int j = i+1 ; j<n; j++){
-                if(arr[i] > arr[j] ){
-                     position = arr[j];
-                     break;
-                }
-            }
-             ans.push_back(position);
-        }
-        return ans;
+        vector<int>ans(n);
+       stack<int>st;
+       st.push(-1);
+       
+       for(int i = n-1 ; i >=0 ; i--){
+           
+           while(st.top() != -1 && st.top() >= arr[i]){
+               st.pop();
+           }
+           ans[i] = st.top();
+           st.push(arr[i]);
+       }
+       return ans;
     } 
 };
 
