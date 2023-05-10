c42c7edb6b2c96480804f706a0c5380bea1909c2
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-05-10 10:33:32 +0530

    Time: 4 ms (33.05%), Space: 6.5 MB (90.43%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-
     vector<vector<int>> generateMatrix(int n) {
+        int startingRow = 0 ,endingRow = n-1, startingCol =0  , endingCol = n-1;
+        int totalElements = n*n , count =1;
+        vector<vector<int>>ans(n,vector<int>(n,-1));
 
-        vector<vector<int>> result (n, vector<int>(n));
-        int cnt = 1;
-        int startingRow =0, startingCol =0 , endingRow = n-1 , endingCol = n-1;
-        
-        while(cnt<=n*n){
-         for(int i = startingCol  ; i<=endingCol ; i++){
-             result[startingRow][i] = cnt++;
-         }
-         startingRow++;
-            
-        for(int i = startingRow ; i<=endingRow ; i++){
-            result[i][endingCol] = cnt++;
-        }
-        endingCol--;
-         
-        for(int i=endingCol; i>=startingCol; i--){
-            result[endingRow][i] = cnt++;
-        }
-        endingRow--;
-        
-        for(int i=endingRow ; i>=startingRow ; i--){
-            result[i][startingCol] = cnt++;
-        }
-         
-        startingCol++;
-            
-        }
+        while(count <= totalElements){
+            //start row
+            for(int col =startingCol; col<=endingCol && count <= totalElements ; col++){
+                ans[startingRow][col] = count;
+                count++;
+            }
+            startingRow++;
 
-        return result;
+
+            
+            //end col
+            for(int row = startingRow; row<=endingRow && count <= totalElements  ; row++){
+                ans[row][endingCol] = count;
+
+                count++;
+            }
+            endingCol--;
+        
+
+            
+            //end row
+            for(int col = endingCol; col>=startingCol && count <= totalElements ; col--){
+                ans[endingRow][col] = count;
+                count++;
+            }
+             endingRow--;
+ 
+            
+            // start col 
+            for(int row = endingRow ; row>=startingRow && count <= totalElements  ; row--){
+                ans[row][startingCol] = count;
+                count++;
+            }
+            startingCol++;
+
+        }
+        return ans;
     }
 };
