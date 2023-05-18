37a6dd8e39386363cc672da88ef4ea53633f78f4
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-05-18 23:46:10 +0530

    Time: 528 ms (5.00%), Space: 129.9 MB (5.00%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -4,13 +4,16 @@ public:
         vector<int>ans;
         
         unordered_map<int,int>indegree;
-        
+        unordered_map<int,bool>visited;
         for(auto edge : edges){
             indegree[edge[1]]++;
+            //for disconected components
+            visited[edge[1]] = true;
+            visited[edge[0]] = true;
         }
         
         for(int i=0; i<n;i++){
-            if(indegree[i] == 0) ans.push_back(i);
+            if(!visited[i] ||  indegree[i] == 0) ans.push_back(i);
         }
         return ans;
     }
