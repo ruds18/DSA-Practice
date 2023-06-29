ccd8c4326d3fe74e430ca292a5af0abd0d0f19c1
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-06-29 15:40:58 +0530

    Added solution - LeetHub

--- Missing element of AP - GFG/missing-element-of-ap.cpp
+++ Missing element of AP - GFG/missing-element-of-ap.cpp
@@ -12,9 +12,9 @@ public:
     int findMissing(int arr[], int n) {
         int diff = (arr[n-1] - arr[0])/n;
         
-        for(int i=0; i<n;i++){
-            if(arr[i] + diff != arr[i+1]){
-                return (arr[i] + diff);
+        for(int i=1; i<n;i++){
+            if((arr[i-1] + diff) != arr[i]){
+                return (arr[i-1] + diff);
             }
         }
         return -1;
