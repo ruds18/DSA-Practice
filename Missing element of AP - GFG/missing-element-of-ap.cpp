6fcc6464d79c5b3d1c086d9d6b2ea606214d3a1f
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-06-29 15:33:12 +0530

    Added solution - LeetHub

---
+++ Missing element of AP - GFG/missing-element-of-ap.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int findMissing(int arr[], int n) {
+        int diff = (arr[n-1] - arr[0])/n;
+        
+        for(int i=0; i<n;i++){
+            if(arr[i] + diff != arr[i+1]){
+                return (arr[i] + diff);
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMissing(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
