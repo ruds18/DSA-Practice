c3ad6067993595234f5250cb183003c01bcea39f
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-06-29 20:38:38 +0530

    Time: 377 ms (5.00%), Space: 364.6 MB (5.04%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
+    
+    int solve(vector<int>piles , vector<int>suffixSum , int index , int m , vector<vector<int>>&dp){
+        //base case
+        if(index >= piles.size()) return 0;
+        if(index + 2*m >= piles.size()) return suffixSum[index];
+        if(dp[index][m] != -1) return dp[index][m];
+        int result =0;
+        for(int x=1 ; x <= 2*m ; x++){
+     result  = max(result , suffixSum[index] - solve(piles , suffixSum , index+x , max(x,m),dp));
+        }
+        return  dp[index][m] =  result;
+    }
+    
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector dp(2, vector(n + 1, vector<int>(n + 1, -1)));
+        int m=1;
         
-        function<int(int, int, int)> f = [&](int p, int i, int m) -> int {
-            if (i == n) {
-                return 0;
-            }
-            if (dp[p][i][m] != -1) {
-                return dp[p][i][m];
-            }
-            int res = p == 1 ? 1000000 : -1, s = 0;
-            for (int x = 1; x <= min(2 * m, n - i); x++) {
-                s += piles[i + x - 1];
-                if (p == 0) {
-                    res = max(res, s + f(1, i + x, max(m, x)));
-                }
-                else {
-                    res = min(res, f(0, i + x, max(m, x)));
-                }
-            }
-            return dp[p][i][m] = res;
-        };
+        vector<vector<int>>dp(n ,  vector<int>(n,-1));
         
-        return f(0, 0, 1);
+        vector<int>suffixSum(n);
+        suffixSum[n-1] = piles[n-1];
+        for(int i = n-2;i>=0; i--){
+            suffixSum[i] = suffixSum[i+1] + piles[i];
+        }
+        
+        return solve(piles , suffixSum , 0 , m,dp);
     }
 };
