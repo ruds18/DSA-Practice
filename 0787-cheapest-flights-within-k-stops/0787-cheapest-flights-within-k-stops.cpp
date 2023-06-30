2c3289098523aab80614b58b62ed4dc030a6fd52
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-06-30 16:24:52 +0530

    Time: 20 ms (90.29%), Space: 13.6 MB (41.24%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-          // create adjacency list
-        unordered_map<int,vector<pair<int, int>>> adjList;
-        for( auto f : flights )
-            adjList[f[0]].push_back( { f[1], f[2] } );
+    
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>>adjList(n);
         
-        // minHeap based on cost of distance from source
-        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> > minHeap;
-        minHeap.push( { 0, src, 0 } ); // cost, vertex, hops
-        
-        vector<int> dist(n+1, INT_MAX); //to avoid TLE
-        
-        while( !minHeap.empty() ) {
-            auto t = minHeap.top(); minHeap.pop();
-            int cost = t[0];
-            int curr = t[1];
-            int stop = t[2];
-            
-            if( curr == dst )
-                return cost;
-            
-                //Optimization to avoid TLE
-            if(dist[curr]<stop) continue;
-                 dist[curr]=stop;
-
-            if(stop >K ) continue;
-            
-                for( auto next : adjList[curr] )
-                    minHeap.push( { cost+next.second, next.first, stop+1 });
+        for(auto e:flights){
+            adjList[e[0]].push_back({e[1] , e[2]});
         }
-        return -1;
+        
+        vector<int>dist(n,INT_MAX);
+        queue<pair<int,int>>q;
+        q.push({src,0});
+        int stops =0;
+        
+        while(!q.empty() && stops<=k){
+            int size = q.size();
+            
+            while(size--){
+                auto [node , distance] = q.front();
+                q.pop();
+                
+                for(auto& [neighbour, price] : adjList[node]){
+                    if(price + distance < dist[neighbour]){
+                        dist[neighbour] = price+distance;
+                        q.push({neighbour, dist[neighbour]});
+                    }
+                }
+            }
+            stops++;
+        }
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
     }
 };
