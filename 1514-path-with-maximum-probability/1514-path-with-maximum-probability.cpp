eed032a0fd01de63cd0aeb577b2677caaafc1cf3
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2023-06-30 14:52:55 +0530

    Time: 246 ms (19.19%), Space: 70 MB (16.27%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        //adjList
-        vector<pair<int,double>>adjList[n]; //Creating adjacency list
-        for(int i=0;i<edges.size();i++){
-            adjList[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adjList[edges[i][1]].push_back({edges[i][0],succProb[i]});
+        //adj List
+        unordered_map<int, vector<pair<int,double>>>adjList;
+        for(int i=0; i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double prob = succProb[i];
+            
+            adjList[u].push_back({v, prob});
+            adjList[v].push_back({u,prob});
+            
         }
-                
+        
+        //max heap
         priority_queue<pair<double,int>>pq;
-        vector<double>probability(n,-1e9);
-        probability[start] =1.0;
         pq.push({1.0, start});
+        vector<double>probability(n,-1e9);
         
         while(!pq.empty()){
-            auto node = pq.top().second;
-            auto prob = pq.top().first;
+            int node = pq.top().second;
+            double prob = pq.top().first;
             pq.pop();
-            for(auto itr : adjList[node]){
-                auto adjNode = itr.first;
-                auto adjProb  = itr.second;
+            for(auto itr: adjList[node]){
+                int adjNode = itr.first;
+                double adjProb = itr.second;
                 
-                if(prob * adjProb > probability[adjNode]){
+                if(prob*adjProb > probability[adjNode]){
                     probability[adjNode] = prob*adjProb;
-                    
                     pq.push({probability[adjNode] , adjNode});
                 }
             }
         }
-        
         if(probability[end] != -1e9) return probability[end]; else return 0;
     }
 };
